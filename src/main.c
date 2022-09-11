a801b1b50a1c5d1e4b87b31cfc9ec65b273eda7f
Donghao Huang <20307130013@fudan.edu.cn>
2022-09-11 12:25:58 +0800

    commit dev1-1

--- src/main.c
+++ src/main.c
@@ -1,8 +1,28 @@
 #include <aarch64/intrinsic.h>
+#include <kernel/init.h>
+#include <driver/uart.h>
+#include <common/string.h>
 
 static char hello[16];
 
+define_early_init(hello){
+    strncpy(hello, "Hello world!", 16);
+}
+
+define_init(print){
+    for(char *p = hello; *p; p++){
+        uart_put_char(*p);
+    }
+}
+
 NO_RETURN void main()
 {
+    extern char etext[], data[], edata[], end[];
+    memset(edata, 0, end - edata);
+    
+    if(cpuid() == 0){
+        do_early_init();
+        do_init();
+    }
     arch_stop_cpu();
 }
